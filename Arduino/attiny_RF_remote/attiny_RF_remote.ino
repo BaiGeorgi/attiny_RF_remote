0375a4860c058028d3f770745c3274f2766caca8
Георги <georgi.patsev@gmail.com>
2022-07-02 06:16:48 +0300

    First commit

---
+++ Arduino/attiny_RF_remote/attiny_RF_remote.ino
@@ -0,0 +1,61 @@
+#include <RCSwitch.h>        // https://github.com/sui77/rc-switch
+RCSwitch mySwitch = RCSwitch();
+const int buttonPin = 2;    // the number of the pushbutton pin
+const int ledPin = 1;      // the number of the LED pin
+
+int ledState = HIGH;         // the current state of the output pin
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+void setup() {
+  mySwitch.enableTransmit(0);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(ledPin, ledState); // set initial LED state
+}
+
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+        //send the RC code
+        mySwitch.send(6662225, 24);
+        delay(100);
+      }
+      if (buttonState == LOW) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  // set the LED:
+  digitalWrite(ledPin, ledState);
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+}
